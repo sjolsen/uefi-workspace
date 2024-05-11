ae3d0d78635b392c28b280f6f750292fdf4f1192
Stuart Olsen <stuart@sj-olsen.com>
2024-05-11 01:36:36 -0500

    Clean up workspace-relative paths

--- setup.lisp
+++ setup.lisp
@@ -1,18 +1,11 @@
 ;; TODO: vendor dependencies
 (ql:quickload '(:clunit :flexi-streams))
 
-;; TODO: compute repository base path
 (asdf:initialize-source-registry
  '(:source-registry
    :ignore-inherited-configuration
-   (:tree "~/Code/uefi-workspace/refinery/BoraxPkg/Lisp")))
+   (:tree (:here "refinery/BoraxPkg/Lisp"))))
 
-;; TODO: put this in the refinery repository and symbol-call it from here
 (defun regenerate-test-data ()
-  (asdf:load-system :borax-runtime/object-file-test)
-  (let* ((test-base "~/Code/uefi-workspace/refinery/BoraxPkg/Test/BoraxRuntimeTest/")
-         (test-files `(("TestFileIA32.bxo" . ,(uiop:symbol-call :borax-runtime/memory :make-memory-model 32 :little-endian))
-                       ("TestFileX64.bxo"  . ,(uiop:symbol-call :borax-runtime/memory :make-memory-model 64 :little-endian)))))
-    (loop for (basename . memory-model) in test-files
-          for path = (concatenate 'string test-base basename)
-          do (uiop:symbol-call :borax-runtime/object-file-test :make-test-file path memory-model))))
+  (asdf:load-system :borax-build/c-testing)
+  (uiop:symbol-call :borax-build/c-testing :make-test-files))
