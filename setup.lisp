080ad20ddb9f7683e35fdd838d8c3fd70e843b7b
Stuart Olsen <stuart@sj-olsen.com>
2024-05-10 23:54:17 -0500

    Fix loading setup.lisp before borax-runtime

--- setup.lisp
+++ setup.lisp
@@ -7,11 +7,12 @@
    :ignore-inherited-configuration
    (:tree "~/Code/uefi-workspace/refinery/BoraxPkg/Lisp")))
 
+;; TODO: put this in the refinery repository and symbol-call it from here
 (defun regenerate-test-data ()
   (asdf:load-system :borax-runtime/object-file-test)
   (let* ((test-base "~/Code/uefi-workspace/refinery/BoraxPkg/Test/BoraxRuntimeTest/")
-         (test-files `(("TestFileIA32.bxo" . ,(borax-runtime/memory:make-memory-model 32 :little-endian))
-                       ("TestFileX64.bxo"  . ,(borax-runtime/memory:make-memory-model 64 :little-endian)))))
+         (test-files `(("TestFileIA32.bxo" . ,(uiop:symbol-call :borax-runtime/memory :make-memory-model 32 :little-endian))
+                       ("TestFileX64.bxo"  . ,(uiop:symbol-call :borax-runtime/memory :make-memory-model 64 :little-endian)))))
     (loop for (basename . memory-model) in test-files
           for path = (concatenate 'string test-base basename)
-          do (borax-runtime/object-file-test:make-test-file path memory-model))))
+          do (uiop:symbol-call :borax-runtime/object-file-test :make-test-file path memory-model))))
