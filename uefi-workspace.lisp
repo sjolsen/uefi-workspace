c646876dc04b933a0399fa0a5d72b9f2a4c11d25
Stuart Olsen <stuart@sj-olsen.com>
2024-08-28 01:57:28 -0500

    Don't put generated files in the source tree

--- uefi-workspace.lisp
+++ uefi-workspace.lisp
@@ -1,6 +1,7 @@
 (uiop:define-package :uefi-workspace
-  (:use :uiop/common-lisp)
-  (:use-reexport :uefi-workspace/edk2 :borax-build/c-testing)
+  (:mix :uiop/common-lisp :borax-virtual-machine/image)
+  (:use :borax-build/c-testing)
+  (:use-reexport :uefi-workspace/edk2)
   (:export #:reload #:test-c #:test-lisp #:test))
 
 (in-package :uefi-workspace)
@@ -8,22 +9,26 @@
 (defun reload ()
   (asdf:load-system :uefi-workspace))
 
-(defun test-c ()
-  (let ((arch-files (make-test-files)))
-    (loop for (arch . test-file) in arch-files
-          for test-bin = (uiop:merge-pathnames*
-                           (format nil "Borax/DEBUG_GCC/~A/BoraxVirtualMachineTest"
-                                   (string arch))
-                           *build-dir*)
-          do (build #P"BoraxPkg/BoraxPkg.dsc" :arch arch)
-          do (uiop:run-program (list "valgrind" "--error-exitcode=1"
-                                     (namestring test-bin)
-                                     (namestring test-file))
-                               :error-output t))))
+(defun test-c (arch memory-model)
+  (let* ((test-base (uiop:merge-pathnames*
+                     (format nil "Borax/DEBUG_GCC/~A/" (string arch))
+                     *build-dir*))
+         (test-bin (uiop:merge-pathnames* #P"BoraxVirtualMachineTest" test-base))
+         (test-file (uiop:merge-pathnames* #P"TestFile.bxo" test-base)))
+    (make-test-file test-file memory-model)
+    (build #P"BoraxPkg/BoraxPkg.dsc" :arch arch)
+    (uiop:run-program (list "valgrind" "--error-exitcode=1"
+                            (namestring test-bin)
+                            (namestring test-file))
+                      :output t
+                      :error-output t)))
 
 (defun test-lisp ()
   (asdf:test-system :borax-virtual-machine))
 
 (defun test ()
   (test-lisp)
-  (test-c))
+  (fresh-line)
+  (test-c :IA32 +32-bit+)
+  (test-c :X64 +64-bit+)
+  (values))
