811158c9f27f8f345a7b736db8f3e1ef59bbcdbe
Stuart Olsen <stuart@sj-olsen.com>
2024-08-28 19:15:22 -0500

    Move run/debug to lisp

--- uefi-workspace.lisp
+++ uefi-workspace.lisp
@@ -5,6 +5,7 @@
   (:export #:reload
            #:uncrustify
            #:build-basetools #:build-ovmf #:build-refinery
+           #:run-qemu
            #:test-c #:test-lisp #:test))
 
 (in-package :uefi-workspace)
@@ -25,6 +26,34 @@
 
 (defun build-refinery ()
   (build #P"RefineryPkg/RefineryPkg.dsc"))
+
+(defun run-qemu (&key debug)
+  (let ((hda (join *build-dir* #P"hda/")))
+    (flet ((symlink (old new)
+             (unless (uiop:file-exists-p new)
+               (ensure-directories-exist new)
+               (sb-posix:symlink old new))))
+      (symlink (join *build-dir* #P"Refinery/DEBUG_GCC/X64/Refinery.efi")
+               (join hda #P"EFI/BOOT/BOOTx64.efi"))
+      (symlink (join *build-dir* #P"Refinery/DEBUG_GCC/X64/UsbMouseDxe.efi")
+               (join hda #P"EFI/Drivers/USBMouseDxe.efi")))
+    (let* ((bios (join *build-dir* #P"OvmfX64/DEBUG_GCC/FV/OVMF.fd"))
+           (args (list "qemu-system-x86_64"
+                       "-net" "none"
+                       "-device" "VGA,xres=640,yres=480"
+                       "-display" "gtk,gl=es"
+                       "-usb"
+                       "-device" "usb-mouse"
+                       "-drive" (format nil "if=pflash,unit=0,format=raw,file=~A" (namestring bios))
+                       "-drive" (format nil "format=raw,file=fat:~A" (namestring hda))
+                       "-snapshot"))
+           (debug-file (join *build-dir* #P"debug.log"))
+           (debug-args (list "-debugcon" (format nil "file:~A" (namestring debug-file))
+                             "-global" "isa-debugcon.iobase=0x402")))
+      (when debug
+        (setf args (nconc args debug-args)))
+      (run-program args :error-output t)))
+  (values))
 
 (defun test-c (arch memory-model)
   (let* ((test-base (join *build-dir* (format nil "Borax/DEBUG_GCC/~A/" (string arch))))
